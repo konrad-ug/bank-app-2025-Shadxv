0af870d9afde0c97b7af886fcf98b140ebc9c5c4
Shadow6328 <kamil_0805@wp.pl>
2025-10-15 15:10:38 +0200

    Added pesel to account

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,10 +3,14 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", None)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
 
     def test_account_balance(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", None)
         assert account.balance == 0
+
+    def test_account_pesel(self):
+        account = Account("John", "Doe", "01010101010")
+        assert account.pesel == "01010101010"
