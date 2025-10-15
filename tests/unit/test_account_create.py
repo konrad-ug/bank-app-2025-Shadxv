b320614cb22b77d1e968d9580e984e846fcb10e3
Shadow6328 <kamil_0805@wp.pl>
2025-10-15 15:24:55 +0200

    Added promo codes

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -22,3 +22,17 @@ class TestAccount:
         assert account2.pesel == "Invalid"
         account3 = Account("John", "Baker", "12345678909")
         assert account3.pesel == "12345678909"
+
+    def test_promo_code(self):
+        account1 = Account("John", "Doe", None, None)
+        assert account1.balance == 0
+        account2 = Account("John", "Doe", None, "PROM_ABC")
+        assert account2.balance == 50
+        account3 = Account("John", "Doe", None, "PROM-ABC")
+        assert account3.balance == 0
+        account4 = Account("John", "Doe", None, "PROM_ABCD")
+        assert account4.balance == 0
+        account5 = Account("John", "Doe", None, "PROM_AB")
+        assert account5.balance == 0
+        account6 = Account("John", "Doe", None, "PRO-ABC")
+        assert account6.balance == 0
