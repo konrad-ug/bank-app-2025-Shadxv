7e97608f71f029929bb4bb798d91b87c23738d87
Shadow6328 <kamil_0805@wp.pl>
2025-10-22 15:05:24 +0200

    Added firm accounts and refactored code for personal accounts

--- src/account.py
+++ src/account.py
@@ -1,14 +1,6 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel, promo_code=None):
-        self.first_name = first_name
-        self.last_name = last_name
+    def __init__(self):
         self.balance = 0
-        if isinstance(pesel, str) and len(pesel) == 11 and pesel.isdigit():
-            self.pesel = pesel
-        else:
-            self.pesel = "Invalid"
-        if isinstance(promo_code, str) and promo_code.startswith("PROM_") and len(promo_code) == 8 and self.is_born_after_1960(self.pesel):
-            self.balance += 50
 
     def transfer_in(self, amount):
         if amount > 0:
@@ -18,6 +10,18 @@ class Account:
         if amount > 0 and self.balance >= amount:
             self.balance -= amount
 
+class PersonalAccount(Account):
+    def __init__(self, first_name, last_name, pesel, promo_code=None):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        if isinstance(pesel, str) and len(pesel) == 11 and pesel.isdigit():
+            self.pesel = pesel
+        else:
+            self.pesel = "Invalid"
+        if isinstance(promo_code, str) and promo_code.startswith("PROM_") and len(promo_code) == 8 and self.is_born_after_1960(self.pesel):
+            self.balance += 50
+
     def is_born_after_1960(self, pesel):
         birth_year_str = pesel[0:2]
         try:
@@ -25,3 +29,12 @@ class Account:
             return birth_year > 60 or (birth_year < 25)
         except:
             return False
+
+class FirmAccount(Account):
+    def __init__(self, company_name, nip):
+        super().__init__()
+        self.company_name = company_name
+        if nip and len(nip) == 10 and nip.isdigit():
+            self.nip = nip
+        else:
+            self.nip = "Invalid"
--- tests/unit/test_account_operations.py
+++ tests/unit/test_account_operations.py
@@ -1,8 +1,8 @@
-from src.account import Account
+from src.account import PersonalAccount, FirmAccount
 
 class TestOperations:
     def test_transfer_in(self):
-        account = Account("John", "Doe", None)
+        account = PersonalAccount("John", "Doe", None)
         account.transfer_in(1000)
         assert account.balance == 1000
         account.transfer_in(-1000)
@@ -13,7 +13,7 @@ class TestOperations:
         assert account.balance == 1100
 
     def test_transfer_out(self):
-        account = Account("John", "Doe", None)
+        account = PersonalAccount("John", "Doe", None)
         account.transfer_in(1000)
         account.transfer_out(100)
         assert account.balance == 900
@@ -21,5 +21,21 @@ class TestOperations:
         assert account.balance == 900
         account.transfer_out(0)
         assert account.balance == 900
+        account.transfer_out(1000)
+        assert account.balance == 900
         account.transfer_out(100)
         assert account.balance == 800
+
+    def test_firm_account(self):
+        account = FirmAccount("Apple Inc.", "1234567890")
+        assert account.company_name == "Apple Inc."
+        assert account.nip == "1234567890"
+        assert account.balance == 0
+        account2 = FirmAccount("Apple Inc.", "123")
+        assert account2.nip == "Invalid"
+        account3 = FirmAccount("Apple Inc.", "123456789098")
+        assert account3.nip == "Invalid"
+        account4 = FirmAccount("Apple Inc.", None)
+        assert account4.nip == "Invalid"
+        account5 = FirmAccount("Apple Inc.", "abcdefghij")
+        assert account5.nip == "Invalid"
